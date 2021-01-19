d839fc22061fad8257c0f310ce4543342cadcd99
Denis <denis.denisenko@gmail.com>
2021-01-19 20:12:48 +0200

    updated1

--- main.py
+++ main.py
@@ -3,17 +3,30 @@ from mysql.connector import Error
 
 
 def get_connection():
+    """
+    function to establish connection with database
+    :return:
+    """
     my_connection = mysql.connector.connect(
         host="localhost", user="root", password="1111", database="Project_3")
     return my_connection
 
 
 def close_connection(some_connection):
+    """
+    function to close the connection with database
+    :param some_connection:
+    :return:
+    """
     if some_connection:
         some_connection.close()
 
 
 def read_database_version():
+    """
+    this function prints the database version
+    :return:
+    """
     global new_connection, my_cursor
     try:
         new_connection = get_connection()
@@ -175,21 +188,32 @@ def update_doctor_experience(exp, doctor_id):
             print("MySQL connection is closed")
 
 
-update_doctor_experience(13, 102)
-
-def doctors_in_hospital (hospital_name):
+def new_table_doctors_in_hospital(hospital_name):
     global connection, cursor
     try:
         connection = get_connection()
         cursor = connection.cursor()
-        sql_select_query = 'Create table  *****   %s;'
+        sql_select_query = ' select hospital_ID from hospital  where hospital_name = %s;'
         cursor.execute(sql_select_query, (hospital_name,))
 
-        sql_select_query = 'Select * From Doctor Where Doctor_ID = %s'
-        cursor.execute(sql_select_query, (hospital_name,))
+        # Extracting Hospital_ID from the given name
+        record = cursor._fetch_row()
+        fetched_hospital_id = record[0]
+        print(fetched_hospital_id)
+
+        # Choose different table name
+        sql_select_query = 'CREATE TABLE new_table ' \
+                           'SELECT Doctor_ID, Doctor_Name, Hospital_ID,Joining_Date,Speciality,Salary,Experience ' \
+                           'FROM Doctor ' \
+                           'WHERE Hospital_ID = %s'
+        cursor.execute(sql_select_query, (fetched_hospital_id,))
+
+        sql_select_query = 'Select * From new_table'
+        cursor.execute(sql_select_query)
 
         record = cursor.fetchall()
 
+        print("Doctors of new table are " + "\n")
         for row in record:
             print("Doctor_ID = ", row[0], )
             print("Doctor_Name = ", row[1])
@@ -208,3 +232,6 @@ def doctors_in_hospital (hospital_name):
             cursor.close()
             connection.close()
             print("MySQL connection is closed")
+
+
+new_table_doctors_in_hospital("Cleveland Clinic")
